aa5f8f0c47d01a4a37c0411abeda3a858ca0dee5
Erin-Louise <2314064c@student.gla.ac.uk>
2020-05-24 17:51:15 +0100

    added basic functionality - gets playlist and writes to file

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-##
+import spotipy
+import sys
+import spotipy.util as util
+
+from spotipy.oauth2 import SpotifyClientCredentials
+def print_tracks(results):
+    for i, item in enumerate(results['items']):
+        track = item['track']
+        print(
+            "Track: %d Artist: %32.32s Track Name: %s ID: %s" %
+            (i + 1 , track['artists'][0]['name'], track['name'],track['id']))
+
+def tracks_to_file(results):
+    f = open("demo.txt","a")
+    for i, item in enumerate(results['items']):
+        track = item['track']
+        f.write(
+            "Track: %d Artist: %32.32s Track Name: %s ID: %s \n" %
+            (i + 1 , track['artists'][0]['name'], track['name'],track['id']))
+        
+
+   
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Whoops, need your username!")
+        print("usage: python user_playlists_contents.py [username]")
+        sys.exit()
+
+    token = util.prompt_for_user_token(username)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['name'] == 'On Repeat':
+                results = sp.playlist(playlist['id'], fields="tracks,next")
+                
+                tracks = results['tracks']
+                print_tracks(tracks)
+                tracks_to_file(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    print_tracks(tracks)
+                    tracks_to_file(tracks)
+                 
+    else:
+        print("Can't get token for", username)
